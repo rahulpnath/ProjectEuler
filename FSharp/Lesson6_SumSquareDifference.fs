ea48011ae77e8ec3a914022f344742259d03b720
Rahul P Nath <rahulpnath@gmail.com>
2018-06-27 16:13:37 +1000

    Lesson 6 F#

---
+++ FSharp/Lesson6_SumSquareDifference.fs
@@ -0,0 +1,13 @@
+﻿module Lesson6_SumSquareDifference
+
+let numbers = [1..100]
+
+let combinations1 = Seq.allPairs numbers numbers
+
+let sqr a = a * a
+let sumOfSquares = numbers |> Seq.sumBy sqr
+                   
+let combinationsSum = Seq.allPairs numbers numbers
+                   |> Seq.sumBy (fun (a,b) -> a * b)
+                   
+let sumSquareDifference = combinationsSum-sumOfSquares
